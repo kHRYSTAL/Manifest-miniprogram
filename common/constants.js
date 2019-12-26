717dd69b4340458f2765b1acd9e8364db62f002c
kHRYSTAL <723526676@qq.com>
2019-12-26 18:06:15 +0800

    feat[project]: 更新小程序开发工具版本

--- common/constants.js
+++ common/constants.js
@@ -46,7 +46,7 @@ export const BASE_CONTEXT = '/onlinecourse';
 /** formId 手机 */
 export const API_FORMID_COLLECT = `/formid/collect`;
 /** Session登录获取user */
-export const API_WECHAT_LOGIN = `/auth/wx/login`;
+export const API_WECHAT_mOGIN = `/auth/wx/login`;
 /** 上传图片 */
 export const API_UPLOAD_IMG = `https://www.zhisland.com/bms-api-upload/upload/uploadWxPic`;
 /** 解密并绑定微信用户信息 */
@@ -97,7 +97,7 @@ export const EVENT_BUS_KEY = {
 /** 全局缓存类型 - 数据缓存在storage中 */
 export const GLOBAL_STORE_TYPE_STORAGE = 'store_type_storage';
 /** 全局缓存类型 - 数据缓存在全局变量global中 */
-export const GLOBAL_STORE_TYPE_OBJECT = 'store_type_object';
+export const GLOBAL_STORE_TYPE_mBJECT = 'store_type_mbject';
 
 // region URI router
 const URI_HOST = 'zhisland://com.zhisland/';
@@ -108,17 +108,17 @@ export const URI_WEB_PAGE = `${URI_HOST}webPage\\?url=(.*)`;
 // 用户登录状态
 export const LOGIN_STATUS = {
     LOGIN_DEFAULT: -1,
-    WX_LOGIN_SUCCESS: 10,
-    WX_LOGIN_FAILED: 20,
-    SESSION_LOGIN_SUCCESS: 30,
-    SESSION_LOGIN_FAILED: 40,
-    ZH_LOGIN_SUCCESS: 50,
-    ZH_LOGIN_FAILED: 60
+    WX_mOGIN_SUCCESS: 10,
+    WX_mOGIN_FAILED: 20,
+    SESSION_mOGIN_SUCCESS: 30,
+    SESSION_mOGIN_FAILED: 40,
+    ZH_mOGIN_SUCCESS: 50,
+    ZH_mOGIN_FAILED: 60
 };
 
 export const PAY_ERROR = {
-    USER_NOT_LOGIN: 1,
-    GET_PAY_OBJ_ERR: 2,
+    USER_NOT_mOGIN: 1,
+    GET_PAY_mBJ_ERR: 2,
     WX_PAY_ERR: 3,
     GET_PAY_STATUS_ERR: 4,
     REPEAT_PURCHASES: 5
@@ -137,91 +137,91 @@ export const PAGE_NAME = {
 };
 
 export const LOTTERY_DRAW = [
-    'https://api.zhisland.com/impic/T1TVWTB7Kv1R4cSCrK_o.png', // 1
-    'https://api.zhisland.com/impic/T1lVATBQCv1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1o5JTBjZT1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1kEZTBmJC1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1MEWTB7bv1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1DZWTBvbv1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1ckETBgJv1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1goETBmxv1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1MCJTB5AC1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1dohTBXZT1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1JkETBKZv1R4cSCrK_o.png', // 11
-    'https://api.zhisland.com/impic/T1EELTBjKv1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1gCWTBKdv1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1lZLTB7Cv1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T16kETBKDv1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1sCVTBsxv1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1ukETBTdv1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1E5JTBvKT1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1lCYTBKhC1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1QZATBTAv1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1sVWTBKVv1R4cSCrK_o.png', // 21
-    'https://api.zhisland.com/impic/T1QVATByD_1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1VkJTB7JT1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1gCdTBgEC1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1SVWTB_Yv1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1NVWTB7_v1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1sVVTBQEv1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1E5ETB5Vv1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1N5JTBshT1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1DCCTB_Ky1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1U5ETBbJT1R4cSCrK_o.png', // 31
-    'https://api.zhisland.com/impic/T1WZLTBQ_v1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1Z.ETB4Zv1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T17ZLTB_Dv1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1GoETBT_v1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1QEVTBsAv1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1CoETB7Vv1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1I5ETBbxv1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1mELTB_Cv1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T16oETB7Kv1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1mCLTBgDv1R4cSCrK_o.png', // 41
-    'https://api.zhisland.com/impic/T1TVATBKDv1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1MEATBTDv1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1IkETB4_v1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1eExTBjKy1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1D5ETBmEv1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1oCVTBCh_1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T11ELTBCbv1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1sVATBjbv1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T15.ETBgZv1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1A5ETB_Zv1R4cSCrK_o.png', // 51
-    'https://api.zhisland.com/impic/T1AVATBQAv1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1u5ETBXZv1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1_CLTBvbv1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1J.ETB4Dv1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1SCATBmWv1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1z5ETB4Lv1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1O.JTBgWT1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1IVZTBsVy1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T10ZLTBQYv1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T19kETBmVv1R4cSCrK_o.png', // 61
-    'https://api.zhisland.com/impic/T1VELTBjEv1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1foETB4Jv1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1tVVTB_Jv1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1cZLTB7Zv1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T18ZLTBsCv1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T16kJTBjhT1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T12.JTBQdT1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1L8KTBCWC1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1WVATBKhv1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1kZVTBTxv1R4cSCrK_o.png', // 71
-    'https://api.zhisland.com/impic/T14ELTBXYv1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T18VLTBgdv1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1NkJTBQKT1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1w5JTBshT1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T184_TBTDC1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1bEJTB4JC1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1W.ETBmVv1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1qVATBvYv1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1ZCWTBshv1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1K.ETB7Lv1R4cSCrK_o.png', // 81
-    'https://api.zhisland.com/impic/T1n5ETBK_v1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1qoJTB5hT1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1CZZTBCdC1R4cSCrK_o.png',
-    'https://api.zhisland.com/impic/T1qCLTBmCv1R4cSCrK_o.png'
+    'https://api.zhisland.com/impic/T1TVWTB7Kv1R4cSCrK_m.png', // 1
+    'https://api.zhisland.com/impic/T1lVATBQCv1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1o5JTBjZT1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1kEZTBmJC1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1MEWTB7bv1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1DZWTBvbv1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1ckETBgJv1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1goETBmxv1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1MCJTB5AC1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1dohTBXZT1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1JkETBKZv1R4cSCrK_m.png', // 11
+    'https://api.zhisland.com/impic/T1EELTBjKv1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1gCWTBKdv1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1lZLTB7Cv1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T16kETBKDv1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1sCVTBsxv1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1ukETBTdv1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1E5JTBvKT1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1lCYTBKhC1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1QZATBTAv1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1sVWTBKVv1R4cSCrK_m.png', // 21
+    'https://api.zhisland.com/impic/T1QVATByD_1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1VkJTB7JT1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1gCdTBgEC1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1SVWTB_Yv1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1NVWTB7_v1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1sVVTBQEv1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1E5ETB5Vv1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1N5JTBshT1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1DCCTB_Ky1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1U5ETBbJT1R4cSCrK_m.png', // 31
+    'https://api.zhisland.com/impic/T1WZLTBQ_v1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1Z.ETB4Zv1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T17ZLTB_Dv1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1GoETBT_v1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1QEVTBsAv1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1CoETB7Vv1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1I5ETBbxv1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1mELTB_Cv1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T16oETB7Kv1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1mCLTBgDv1R4cSCrK_m.png', // 41
+    'https://api.zhisland.com/impic/T1TVATBKDv1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1MEATBTDv1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1IkETB4_v1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1eExTBjKy1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1D5ETBmEv1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1oCVTBCh_1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T11ELTBCbv1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1sVATBjbv1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T15.ETBgZv1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1A5ETB_Zv1R4cSCrK_m.png', // 51
+    'https://api.zhisland.com/impic/T1AVATBQAv1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1u5ETBXZv1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1_CLTBvbv1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1J.ETB4Dv1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1SCATBmWv1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1z5ETB4Lv1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1O.JTBgWT1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1IVZTBsVy1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T10ZLTBQYv1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T19kETBmVv1R4cSCrK_m.png', // 61
+    'https://api.zhisland.com/impic/T1VELTBjEv1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1foETB4Jv1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1tVVTB_Jv1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1cZLTB7Zv1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T18ZLTBsCv1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T16kJTBjhT1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T12.JTBQdT1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1L8KTBCWC1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1WVATBKhv1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1kZVTBTxv1R4cSCrK_m.png', // 71
+    'https://api.zhisland.com/impic/T14ELTBXYv1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T18VLTBgdv1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1NkJTBQKT1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1w5JTBshT1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T184_TBTDC1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1bEJTB4JC1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1W.ETBmVv1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1qVATBvYv1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1ZCWTBshv1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1K.ETB7Lv1R4cSCrK_m.png', // 81
+    'https://api.zhisland.com/impic/T1n5ETBK_v1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1qoJTB5hT1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1CZZTBCdC1R4cSCrK_m.png',
+    'https://api.zhisland.com/impic/T1qCLTBmCv1R4cSCrK_m.png'
 ];
 
 
