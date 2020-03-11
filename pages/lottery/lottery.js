af7d0a096e718063ef5df8a65497bd6b574d205f
kHRYSTAL <723526676@qq.com>
2020-03-11 12:12:44 +0800

    update[lottery]: use bindload to optimize animation display

--- pages/lottery/lottery.js
+++ pages/lottery/lottery.js
@@ -3,11 +3,8 @@ import {LOTTERY_DRAW} from "../../common/constants";
 Page({
     data: {
         dollMachine: LOTTERY_DRAW[0],
-        dollMachinePreLoad1:LOTTERY_DRAW[1],
-        dollMachinePreLoad2:LOTTERY_DRAW[2],
-        dollMachinePreLoad3:LOTTERY_DRAW[3],
-        dollMachinePreLoad4:LOTTERY_DRAW[4],
-        dollMachinePreLoad5:LOTTERY_DRAW[5],
+        dollMachinePreLoad1: LOTTERY_DRAW[1],
+        dollMachinePreLoad2: LOTTERY_DRAW[2],
         currentImageIndex: 0,
         isAnimationRunning: false,
         showPrompt: false
@@ -18,6 +15,19 @@ Page({
 
     onClickMachine() {
         this.startAnimation();
+    },
+
+    onImageShowSuccess() {
+        if (this.data.currentImageIndex === 0) {
+            return;
+        }
+        setTimeout(() => {
+            this.updateDraw();
+        }, 25);
+    },
+
+    onImageShowError() {
+        this.updateDraw();
     },
 
     startAnimation() {
@@ -34,48 +44,30 @@ Page({
             // 初始化样式
             that.setData({
                 dollMachine: LOTTERY_DRAW[0],
-                dollMachinePreLoad1:LOTTERY_DRAW[1],
-                dollMachinePreLoad2:LOTTERY_DRAW[2],
-                dollMachinePreLoad3:LOTTERY_DRAW[3],
-                dollMachinePreLoad4:LOTTERY_DRAW[4],
-                dollMachinePreLoad5:LOTTERY_DRAW[5],
+                dollMachinePreLoad1: LOTTERY_DRAW[1],
+                dollMachinePreLoad2: LOTTERY_DRAW[2],
                 currentImageIndex: 0,
-                isAnimationRunning: false
-            });
-            that.setData({
+                isAnimationRunning: false,
+
                 showPrompt: true,
-                isAnimationRunning: false
             });
-            return;
         } else {
             that.data.currentImageIndex++;
             let index = that.data.currentImageIndex;
             let index2 = index + 1;
             let index3 = index2 + 1;
-            let index4 = index3 + 1;
-            let index5 = index4 + 1;
-            let index6 = index5 + 1;
-            if (index6 > LOTTERY_DRAW.length - 1) {
-                index2 = LOTTERY_DRAW.length - 5;
-                index3 = LOTTERY_DRAW.length - 4;
-                index4 = LOTTERY_DRAW.length - 3;
-                index5 = LOTTERY_DRAW.length - 2;
-                index6 = LOTTERY_DRAW.length - 1;
-
+            if (index3 > LOTTERY_DRAW.length - 1) {
+                index2 = LOTTERY_DRAW.length - 2;
+                index3 = LOTTERY_DRAW.length - 1;
             }
             // 执行动画
             that.setData({
                 dollMachine: LOTTERY_DRAW[index],
-                dollMachinePreLoad1:LOTTERY_DRAW[index2],
-                dollMachinePreLoad2:LOTTERY_DRAW[index3],
-                dollMachinePreLoad3:LOTTERY_DRAW[index4],
-                dollMachinePreLoad4:LOTTERY_DRAW[index5],
-                dollMachinePreLoad5:LOTTERY_DRAW[index6],
+                dollMachinePreLoad1: LOTTERY_DRAW[index2],
+                dollMachinePreLoad2: LOTTERY_DRAW[index3],
             });
         }
-        setTimeout(function () {
-            that.updateDraw();
-        }, 50);
+
     },
 
     onOKClick() {
